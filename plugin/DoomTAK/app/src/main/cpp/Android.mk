b6c4e894d2accad0cec4c3dace25ff3bd4d02892
PHernimanXiSystems <pete.herniman@xisystems.co.uk>
2024-10-03 09:08:14 +0100

    Added PureDOOM code.

--- plugin/DoomTAK/app/src/main/cpp/Android.mk
+++ plugin/DoomTAK/app/src/main/cpp/Android.mk
@@ -2,15 +2,19 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-
-LOCAL_CFLAGS=-O3 -std=c++11 -D__GXX_EXPERIMENTAL_CXX0X__
+LOCAL_CFLAGS := -O3 -D__GXX_EXPERIMENTAL_CXX0X__
 LOCAL_CPPFLAGS := -std=c++11
 LOCAL_MODULE := doomtak
+
+# Include DoomTAK C++ code
 LOCAL_SRC_FILES := doomtak.cpp
 
-LOCAL_C_INCLUDES += ${LOCAL_PATH}/../../../build/generated/jni
+# Include all PureDOOM .c files from the DOOM directory
+PUREDOOM_SRC_PATH := $(LOCAL_PATH)/DOOM
+LOCAL_SRC_FILES += $(wildcard $(PUREDOOM_SRC_PATH)/*.c)
+
+LOCAL_C_INCLUDES += $(PUREDOOM_SRC_PATH) ${LOCAL_PATH}/../../../build/generated/jni
+
 LOCAL_LDLIBS := -llog
 
 include $(BUILD_SHARED_LIBRARY)
-
-
--- plugin/DoomTAK/app/src/main/cpp/doomtak.cpp
+++ plugin/DoomTAK/app/src/main/cpp/doomtak.cpp
@@ -1,8 +1,11 @@
 #include <jni.h>
 #include "com_atakmap_android_doomtak_plugin_DoomTakTool.h"
+#include "PureDOOM.h"
 
 JNIEXPORT jstring JNICALL Java_com_atakmap_android_doomtak_plugin_DoomTakTool_myNativeMethod
   (JNIEnv *env, jclass clazz)
 {
+    doom_init(0, nullptr, 0);
+
     return env->NewStringUTF("Hello DoomTAK!");
 }
