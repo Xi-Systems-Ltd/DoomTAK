dab5a6a4af1d10cf6088103114266ddaee6b85a1
PHernimanXiSystems <pete.herniman@xisystems.co.uk>
2024-11-08 14:24:35 +0000

    Removed mouse controls for menu-ing.

--- plugin/DoomTAK/app/src/main/cpp/DOOM/m_menu.c
+++ plugin/DoomTAK/app/src/main/cpp/DOOM/m_menu.c
@@ -1716,34 +1716,6 @@ doom_boolean M_Responder(event_t* ev)
     {
         if (ev->type == ev_mouse && mousewait < I_GetTime())
         {
-            mousey += ev->data3;
-            if (mousey < lasty - 30)
-            {
-                ch = KEY_DOWNARROW;
-                mousewait = I_GetTime() + 5;
-                mousey = lasty -= 30;
-            }
-            else if (mousey > lasty + 30)
-            {
-                ch = KEY_UPARROW;
-                mousewait = I_GetTime() + 5;
-                mousey = lasty += 30;
-            }
-
-            mousex += ev->data2;
-            if (mousex < lastx - 30)
-            {
-                ch = KEY_LEFTARROW;
-                mousewait = I_GetTime() + 5;
-                mousex = lastx -= 30;
-            }
-            else if (mousex > lastx + 30)
-            {
-                ch = KEY_RIGHTARROW;
-                mousewait = I_GetTime() + 5;
-                mousex = lastx += 30;
-            }
-
             if (ev->data1 & 1)
             {
                 ch = KEY_ENTER;
